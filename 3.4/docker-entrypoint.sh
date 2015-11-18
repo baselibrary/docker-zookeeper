5315736d56360765656c606da855cfb7b8aadb3e
Shawn Ma <qsma@thoughtworks.com>
2015-11-18 15:05:18 +0800

    update

--- 3.4/docker-entrypoint.sh
+++ 3.4/docker-entrypoint.sh
@@ -4,7 +4,7 @@
 set -e -m
 
 #####   variables  #####
-: ${ZOO_ID:=1}
+: ${ZOOKEEPER_ID:=1}
 
 # add command if needed
 if [ "${1:0:1}" = '-' ]; then
@@ -17,8 +17,22 @@ if [ "$1" = 'zkServer.sh' ]; then
   echo "========================================================================"
   echo "initialize:"
   echo "========================================================================"
-  mkdir -p /var/lib/zookeeper && echo "${ZOO_ID}" > /var/lib/zookeeper/myid
+  mkdir -p /var/lib/zookeeper && echo "${ZOOKEEPER_ID}" > /var/lib/zookeeper/myid
+  for VAR in `env`; do
+    if [[ $VAR =~ ^ZOOKEEPER_SERVER_[0-9]+= ]]; then
+      SERVER_ID=`echo "$VAR" | sed -r "s/ZOOKEEPER_SERVER_(.*)=.*/\1/"`
+      SERVER_IP=`echo "$VAR" | sed 's/.*=//'`
+      if [ "${SERVER_ID}" = "${ZOOKEEPER_ID}" ]; then
+        echo "server.${SERVER_ID}=0.0.0.0:2888:3888" >> /etc/zookeeper/conf/zoo.cfg
+        echo "server.${SERVER_ID}=0.0.0.0:2888:3888"
+      else
+        echo "server.${SERVER_ID}=${SERVER_IP}:2888:3888" >> /etc/zookeeper/conf/zoo.cfg
+        echo "server.${SERVER_ID}=${SERVER_IP}:2888:3888"
+      fi
+    fi
+  done
 
+  cat /etc/zookeeper/conf/zoo.cfg
   
   ##### run scripts  #####
   echo "========================================================================"
